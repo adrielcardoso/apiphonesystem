d9b053c0c05b49878a4af9e87abb1aeb12ffe324
adriel <adrielcardoso@MacBook-Air-de-Adriel.local>
2019-09-04 22:21:47 -0300

    ws

--- src/main/java/phonesystem/adrielcardoso/com/br/phonesystem/controller/CustomerController.java
+++ src/main/java/phonesystem/adrielcardoso/com/br/phonesystem/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package phonesystem.adrielcardoso.com.br.phonesystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import phonesystem.adrielcardoso.com.br.phonesystem.repository.CustomerRepository;
 import phonesystem.adrielcardoso.com.br.phonesystem.utils.Response;
 
 @CrossOrigin(origins = "*")
@@ -8,11 +10,14 @@ import phonesystem.adrielcardoso.com.br.phonesystem.utils.Response;
 @RequestMapping("/api/v1/customer")
 public class CustomerController
 {
+    @Autowired
+    CustomerRepository customerRepository;
+
     @RequestMapping(value = "", method = RequestMethod.GET)
     Object findAll()
     {
         try{
-            return new Response("", 200);
+            return new Response("", 200, this.customerRepository.findAll());
         }catch(Exception e){
             return new Response(e.getMessage(), 400);
         }
