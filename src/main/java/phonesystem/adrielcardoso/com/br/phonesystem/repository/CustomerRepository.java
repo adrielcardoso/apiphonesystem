5b3e1caf799615dba69251ccceea9832043bb443
Adriel Cardoso <adriel.cardoso@itajai.sc.gov.br>
2019-09-06 18:02:49 -0300

    w

--- src/main/java/phonesystem/adrielcardoso/com/br/phonesystem/repository/CustomerRepository.java
+++ src/main/java/phonesystem/adrielcardoso/com/br/phonesystem/repository/CustomerRepository.java
@@ -1,14 +1,18 @@
 package phonesystem.adrielcardoso.com.br.phonesystem.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import phonesystem.adrielcardoso.com.br.phonesystem.entity.CustomerEntity;
-import java.util.List;
 
 @Repository
 public interface CustomerRepository extends PagingAndSortingRepository<CustomerEntity, Long>
 {
-    List<CustomerEntity> findAllByName(Pageable pageable);
+    @Query(
+            value = "SELECT * FROM customer u WHERE phone REGEXP '\\(237\\)\\ ?[2368]\\d{7,8}$'",
+            nativeQuery = true)
+    Page<CustomerEntity> findAll(Pageable pageable);
 }
 
