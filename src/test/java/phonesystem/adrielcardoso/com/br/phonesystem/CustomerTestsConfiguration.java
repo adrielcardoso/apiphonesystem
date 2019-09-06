a46244ac4a26b267f4c0fd9c6f7632d2916eed66
Adriel Cardoso <adriel.cardoso@itajai.sc.gov.br>
2019-09-06 11:04:42 -0300

    test

--- src/test/java/phonesystem/adrielcardoso/com/br/phonesystem/CustomerTestsConfiguration.java
+++ src/test/java/phonesystem/adrielcardoso/com/br/phonesystem/CustomerTestsConfiguration.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.repository.config.CustomRepositoryImplementationDetector;
-import phonesystem.adrielcardoso.com.br.phonesystem.repository.CustomerRepository;
 import phonesystem.adrielcardoso.com.br.phonesystem.service.CustomerService;
 
 @DisplayName("Config my CustomService")
